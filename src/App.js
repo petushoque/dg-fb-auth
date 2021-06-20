23a802764e69f004a0580910a8754744b97d423d
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-20 21:19:59 +0700

    feat: стейт с информацией о пользователе

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useState, useEffect } from 'react';
 import './App.css';
 import Facebook from './components/Facebook';
+import Header from './components/Header';
+
 
 function App() {
 
+  const [user, setUser] = useState('')
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <Header />
       <Facebook/>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <h1 className='header__title'>Deutsche Grammatik</h1>
+        </header>
+    )
+}
