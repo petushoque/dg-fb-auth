c87806ed5914ea6abaee051eb94e1cd4c20dfec6
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-22 20:51:05 +0700

    feat: создать контекст пользователя

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import './App.css';
+import CurrentUserContext from './contexts/CurrentUserContext';
 import Facebook from './components/Facebook';
 import Header from './components/Header';
 import Footer from './components/Footer'
