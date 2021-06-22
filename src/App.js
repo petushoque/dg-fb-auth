bdf66b8bcff3ffdfd46dc2fe4656694aeddfdd02
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-22 20:52:43 +0700

    feat: создать контекст пользователя

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
 
   return (
     <div className='page'>
-      <Header />
-      <Facebook />
-      <Footer />
+      <CurrentUserContext.Provider> 
+        <Header />
+        <Facebook />
+        <Footer />
+      </CurrentUserContext.Provider> 
     </div>
   );
 }
