e694c96f3e2c503128fe215871b5f3924ea8220d
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-26 21:12:25 +0700

    feat: убрать неиспользуемые зависимости

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 import CurrentUserContext from './contexts/CurrentUserContext';
